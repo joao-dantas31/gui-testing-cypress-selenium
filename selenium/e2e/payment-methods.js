c7c57b63905b178a35e9f297000e5ab03dc61866
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-04-20 16:46:11 -0300

    fix: fixing selenium tests

--- selenium/e2e/payment-methods.js
+++ selenium/e2e/payment-methods.js
@@ -51,27 +51,22 @@ describe('payment methods', () => {
     it('search for a non existent payment method', async () => {
         await driver.findElement(By.linkText('Payment methods')).click();
 
-        await driver.findElement(By.id('criteria_search_value')).sendKeys('credit');
+        await driver.findElement(By.id('criteria_search_value')).sendKeys('test');
         await driver.findElement(By.css('*[class^="ui blue labeled icon button"]')).click();
 
         const bodyText = await driver.findElement(By.tagName('body')).getText();
         assert(bodyText.includes('There are no results to display'));
     });
 
-    it('trying to delete an active payment method', async () => {
+    it('try to clear filters after filtering', async () => {
         await driver.findElement(By.linkText('Payment methods')).click();
 
         await driver.findElement(By.id('criteria_search_value')).sendKeys('cash');
-
         await driver.findElement(By.css('*[class^="ui blue labeled icon button"]')).click();
-
-        const buttons = await driver.findElements(By.css('*[class^="ui red labeled icon button"]'));
-        await buttons[buttons.length - 1].click();
-
-        await driver.findElement(By.css('*[class^="ui green ok inverted button"]')).click();
+        await driver.findElement(By.css('*[class^="icon remove"]')).click();
 
         const bodyText = await driver.findElement(By.tagName('body')).getText();
-        assert(bodyText.includes('Cannot delete, the Payment method is in use.'));
+        assert(bodyText.includes('Bank transfer'));
     });
 
     it('trying to search for bank transfer payment method', async () => {
@@ -85,7 +80,7 @@ describe('payment methods', () => {
         assert(!bodyText.includes('cash_on_delivery'));
     });
 
-    it('trying to disable the payment method', async () => {
+    it('trying to change a payment name', async () => {
         await driver.findElement(By.linkText('Payment methods')).click();
 
         await driver.findElement(By.id('criteria_search_value')).sendKeys('cash');
@@ -95,11 +90,13 @@ describe('payment methods', () => {
         const buttons = await driver.findElements(By.css('*[class^="ui labeled icon button "]'));
         await buttons[buttons.length - 1].click();
 
-        await driver.findElement(By.css("label[for='sylius_payment_method_enabled']")).click();
+        await driver.findElement(By.id('sylius_payment_method_translations_en_US_name')).sendKeys('Credit card');
 
         await driver.findElement(By.id('sylius_save_changes_button')).click();
 
+        await driver.findElement(By.linkText('Payment methods')).click();
+
         const bodyText = await driver.findElement(By.tagName('body')).getText();
-        assert(bodyText.includes('Payment method has been successfully updated.'));
+        assert(bodyText.includes('Credit card'));
     });
 });
